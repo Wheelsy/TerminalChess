01b06beb3ea2ab731b0b9f470f89de9b3466f3a3
Ely Hawkins <44911472+Wheelsy@users.noreply.github.com>
2024-01-14 14:14:52 +1100

    Hardcoded strings
    Added hardcoded print messages

--- TerminalChess/Utils.cs
+++ TerminalChess/Utils.cs
@@ -8,5 +8,44 @@ namespace TerminalChess
 {
     internal class Utils
     {
+        public string welcome =
+            """
+             _/|                    |\_
+            // -\                   /- \\
+            || ._)                 (_. ||
+            //__\                   /__\\
+            )___(  TERMINAL CHESS   )___(
+
+            """;
+
+        public string mainMenu =
+            """
+            ----MAIN MENU----
+            [0] New game
+            [1] Load game
+            [2] Exit application
+            [3] Credits
+
+            """;
+
+        public string optionsMenu =
+            """
+            ----OPTIONS----
+            [0] Save game
+            [1] Exit game
+
+            """;
+
+        public string gameOverMenu =
+            """
+            ----GAME OVER----
+            [0] Menu
+            [1] Exit application
+
+            """;
+
+        public string inputPrompt = ">";
+
+        public string goodbye = "Goodbye =]";
     }
 }
